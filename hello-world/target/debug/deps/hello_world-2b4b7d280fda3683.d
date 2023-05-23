21a283fd3188c97dceda0a1330ff901e3a1e81b6
Gina Beth Russell <ginabethrussell@Ginas-MBP-2.cinci.rr.com>
2023-05-22 21:29:09 -0400

    Adds setup and check to readme

---
+++ hello-world/target/debug/deps/hello_world-2b4b7d280fda3683.d
@@ -0,0 +1,5 @@
+/Users/ginabethrussell/Developer/tdd-katas-rust/hello-world/target/debug/deps/hello_world-2b4b7d280fda3683.rmeta: src/main.rs
+
+/Users/ginabethrussell/Developer/tdd-katas-rust/hello-world/target/debug/deps/hello_world-2b4b7d280fda3683.d: src/main.rs
+
+src/main.rs:
---
+++ hello-world/target/debug/deps/hello_world-695e48265cbd756f.d
@@ -0,0 +1,5 @@
+/Users/ginabethrussell/Developer/tdd-katas-rust/hello-world/target/debug/deps/hello_world-695e48265cbd756f.rmeta: src/main.rs
+
+/Users/ginabethrussell/Developer/tdd-katas-rust/hello-world/target/debug/deps/hello_world-695e48265cbd756f.d: src/main.rs
+
+src/main.rs:
